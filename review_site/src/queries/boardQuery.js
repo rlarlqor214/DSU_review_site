c0bf9871eab66bf9cb31601d499c33f0d00c9298
amazon7737 <76634341+amazon7737@users.noreply.github.com>
2023-07-27 11:47:37 +0900

    Delete review_site directory

--- review_site/src/queries/boardQuery.js
+++
@@ -1,8 +0,0 @@
-exports.selectAllPosts =
-  "select post_uid, post_title, post_writer from review_table";
-exports.selectPostDetail = "select * from review_table where post_uid = ?";
-exports.writePost =
-  "insert into review_table(post_title, post_content, post_writer) values(?, ?, ?)";
-exports.modifyPost =
-  "update review_table set post_title=?, post_content=?, post_writer=? where post_uid=?";
-exports.deletePost = "delete from review_table where post_uid=?";
