1b0e22f52c340b1e4cd6c12bc614bc4a9280e4de
김강민 <amazon7737@gmail.com>
2023-09-05 15:22:04 +0900

    new

---
+++ src/pages/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Test from '../components/main2';
+
+const Main2 = () => {
+    return (
+        <div className="wrap">
+            <Test></Test>
+        </div>
+    );
+};
+
+export default Main2;
